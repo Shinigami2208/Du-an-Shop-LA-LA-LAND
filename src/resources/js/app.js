f20b439f09a4cdb8f38fbbb4cdc476ca5ee2cd8b
Shinigami2208 <bhhoang220898@gmail.com>
2020-05-24 12:00:38 +0700

    Fix app.js

---
+++ src/resources/js/app.js
@@ -0,0 +1,74 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+window.Vue = require('vue');
+
+// axios
+window.axios = require('axios');
+
+// Router
+import router from './Router.js';
+
+// Event Bus
+Vue.prototype.eventBus = new Vue();
+
+// Lodash
+Vue.prototype._ = require('lodash');
+// Rewrite-URL
+Vue.prototype.rewriteUrl = function rewriteUrl(title){
+    var slug = "";
+    // Change to lower case
+    var titleLower = title.toLowerCase();
+    // Letter "e"
+    slug = titleLower.replace(/e|é|è|ẽ|ẻ|ẹ|ê|ế|ề|ễ|ể|ệ/gi, 'e');
+    // Letter "a"
+    slug = slug.replace(/a|á|à|ã|ả|ạ|ă|ắ|ằ|ẵ|ẳ|ặ|â|ấ|ầ|ẫ|ẩ|ậ/gi, 'a');
+    // Letter "o"
+    slug = slug.replace(/o|ó|ò|õ|ỏ|ọ|ô|ố|ồ|ỗ|ổ|ộ|ơ|ớ|ờ|ỡ|ở|ợ/gi, 'o');
+    // Letter "u"
+    slug = slug.replace(/u|ú|ù|ũ|ủ|ụ|ư|ứ|ừ|ữ|ử|ự/gi, 'u');
+    // Letter "i"
+    slug = slug.replace(/ị|ỉ|ì|í|ĩ/gi, 'i');
+    // Letter "y"
+    slug = slug.replace(/ý|ỷ|ỳ|ỵ|ỹ/gi, 'y');
+    // Letter "d"
+    slug = slug.replace(/đ/gi, 'd');
+    // Trim the last whitespace
+    slug = slug.replace(/\s*$/g, '');
+    // Change whitespace to "-"
+    slug = slug.replace(/\s+/g, '-');
+
+    return slug.toUpperCase();
+}
+
+//  Filter
+
+Vue.filter('dinhDangTien', function(soTien, phanCach){
+    return soTien.toString().replace(/\B(?=(\d{3})+(?!\d))/g, phanCach) + '₫';
+});
+
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    router
+});
